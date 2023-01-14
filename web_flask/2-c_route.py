6e523fcb707b3f5dd0a796fee13af95d624e4180
ehoneahobed <ehoneahobed@hotmail.com>
2023-01-14 20:44:12 +0000

    Update solutions to tasks 2, 3, and 4

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,8 +23,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def cText(text):
     """display C followed by the value of the text variable"""
-    text = text.replace('_', ' ')
-    return (f"C {text}")
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,15 +23,13 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def cText(text):
     """display C followed by the value of the text variable"""
-    text = text.replace('_', ' ')
-    return (f"C {text}")
+    return "C {}".format(text.replace("_", " "))
 
 
 @app.route("/python/<text>", strict_slashes=False)
 def pythonText(text="is cool"):
     """display Python followed by the value of the text variable"""
-    text = text.replace('_', ' ')
-    return (f"Python {text}")
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
